7e6a626f3d54431450e386b1c74c16c3a7ed9785
Ihmanuel <114946355+Incognito-1@users.noreply.github.com>
2026-07-03 18:36:49 +0200

    Add files via upload

---
+++ python crashcourse by eric/try 1.2.py
@@ -0,0 +1,12 @@
+#successfully displayed on the terminal.
+print("Hello World!")
+#############
+
+#errors
+print("Hello World!") h
+
+printt("Hello World!") 
+#both functions failed,and gave an error.
+#There are different types of errors but in our case we are only going to 
+# look into those cased by "typos" which are refered to as "syntax error", and "Name error"
+#respectively represented on the above lines of code

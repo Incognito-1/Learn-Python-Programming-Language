b17a92f9cb6e2388e7273e3fd83f34cf5631ba59
Ihmanuel <114946355+Incognito-1@users.noreply.github.com>
2026-07-05 23:36:31 +0200

    Add files via upload

---
+++ python crashcourse by eric/try 2.4.py
@@ -0,0 +1,11 @@
+#created the name variable
+name = "Thomas Edison"
+
+#prints name's value in uppercase using upper function
+print(name.upper())
+
+#prints name's value in lower using lower function
+print(name.lower())
+
+#prints name's value in title using title function 
+print(name.title())
